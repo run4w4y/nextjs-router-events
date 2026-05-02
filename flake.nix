0d7537f68cd5f3c411d0263b1df230a00726bbc6
Vadim Makarov <add4che@gmail.com>
2026-05-02 09:23:38 +0300

    update node version in the flake

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             bun
-            nodejs_22
+            nodejs_latest
             act
             direnv
           ];
